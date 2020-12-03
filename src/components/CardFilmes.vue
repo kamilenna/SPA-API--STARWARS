97c13be09df6b0e71844ac51284621494c4b31ac
kamilenna <karla.milenna1@gmail.com>
2020-12-03 17:28:22 -0300

    Criação do componente CardFilmes e desenvolvendo página filmes

---
+++ src/components/CardFilmes.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+<q-card dark bordered class="bg-grey-9 my-card q-pa-lg q-mb-md row wrap justify-around">
+  <q-card-section>
+    <div class="text-h3">{{title}}</div>
+  </q-card-section>
+
+  <q-separator dark inset />
+
+  <q-card-section>
+    <p>Episódio: {{episode_id}}</p>
+    <p>Abertura: {{opening_crawl}}</p>
+    <p>Diretor: {{director}}</p>
+    <p>Produtores: {{producer}}</p>
+    <p>Data de lançamento: {{release_date}}</p>
+  </q-card-section>
+</q-card>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      filme: {
+        people: '',
+        planet: ''
+      }
+    }
+  },
+  props: {
+    title: String,
+    episode_id: Number,
+    opening_crawl: String,
+    director: String,
+    producer: String,
+    release_date: String,
+    url: String
+  }
+}
+</script>
+
+<style lang="stylus">
+p {
+    color: $warning;
+    font-family: Roboto;
+    font-size: 18px;
+}
+.my-card {
+  width: 100%;
+}
+</style>
--- src/pages/Movies.vue
+++ src/pages/Movies.vue
@@ -1,11 +1,31 @@
+
 <template>
   <q-page padding>
-    <!-- content -->
+    <div v-for="(movie,index) in filmes" :key="index">
+      <Filmes :title="movie.title" :url="movie.url" :episode_id="movie.episode_id" :opening_crawl="movie.opening_crawl" :director="movie.director" :producer="movie.producer" :release_date="movie.release_date"/>
+    </div>
   </q-page>
 </template>
 
 <script>
+import axios from 'axios'
+import Filmes from 'src/components/CardFilmes'
 export default {
-  // name: 'PageName',
+  name: 'PageMovies',
+  data () {
+    return {
+      filmes: []
+    }
+  },
+  created: function () {
+    axios.get('https://swapi.dev/api/films/').then(res => {
+      console.log('Pegou a lista de filmes')
+      this.filmes = res.data.results
+      console.log(this.filmes)
+    })
+  },
+  components: {
+    Filmes
+  }
 }
 </script>
